3822e91a1bedcee0aa8eab0a40f0e9f2fd7107bf
Pablo Zapeta <zapetapablo@gmail.com>
2024-01-26 19:04:56 -0600

    finish refactor of dynamic table

--- lib/views/developer_company/pages/list_companies_page.dart
+++ lib/views/developer_company/pages/list_companies_page.dart
@@ -4,15 +4,18 @@ import 'package:developer_company/data/implementations/CDI/cdi_repository_impl.d
 import 'package:developer_company/data/providers/CDI/cdi_provider.dart';
 // import 'package:developer_company/data/providers/company_provider.dart';
 import 'package:developer_company/data/repositories/CDI/cdi_repository.dart';
-// import 'package:developer_company/data/repositories/company_repository.dart';
-import 'package:developer_company/shared/resources/colors.dart';
-import 'package:developer_company/shared/resources/dimensions.dart';
 import 'package:developer_company/shared/routes/router_paths.dart';
-import 'package:developer_company/widgets/app_bar_sidebar.dart';
-import 'package:developer_company/widgets/data_table.dart';
-import 'package:developer_company/widgets/delete_company_dialog.dart';
-import 'package:developer_company/widgets/filter_box.dart';
-import 'package:developer_company/widgets/layout.dart';
+// import 'package:developer_company/data/repositories/company_repository.dart';
+// import 'package:developer_company/shared/resources/colors.dart';
+// import 'package:developer_company/shared/resources/dimensions.dart';
+// import 'package:developer_company/shared/routes/router_paths.dart';
+// import 'package:developer_company/widgets/CDI/dynamic_form.dart';
+import 'package:developer_company/widgets/CDI/dynamic_table_widget.dart';
+// import 'package:developer_company/widgets/app_bar_sidebar.dart';
+// import 'package:developer_company/widgets/data_table.dart';
+// import 'package:developer_company/widgets/delete_company_dialog.dart';
+// import 'package:developer_company/widgets/filter_box.dart';
+// import 'package:developer_company/widgets/layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
@@ -30,36 +33,36 @@ class _ListCompaniesState extends State<ListCompanies> {
   List<dynamic> companies = [];
   List<dynamic> filteredCompanies = [];
 
-  _getCompanies() async {
-    String COMPANY_ENDPOINT = "orders/v1/getCompanies";
-    final tempCompanies = await cdiProvider.fetchDataList(COMPANY_ENDPOINT);
+  // _getCompanies() async {
+  //   String COMPANY_ENDPOINT = "orders/v1/getCompanies";
+  //   final tempCompanies = await cdiProvider.fetchDataList(COMPANY_ENDPOINT);
 
-    companies = tempCompanies;
-    filteredCompanies = tempCompanies;
-  }
+  //   companies = tempCompanies;
+  //   filteredCompanies = tempCompanies;
+  // }
 
   CDIRepository cdiRepository = CDIRepositoryImpl(CDIProvider());
-
+  final String COMPANY_ENTITY = "1";
+  String COMPANY_ENDPOINT = "orders/v1/getCompanies";
   List<dynamic> columnsData = [];
 
-  _getFormCompany() async {
-    EasyLoading.show();
-    filteredCompanies.clear();
-    companies.clear();
-    final String COMPANY_ENTITY = "1";
-    final result = await cdiRepository.fetchDataTable(COMPANY_ENTITY);
-    columnsData = result
-        .where((e) => e["ShowInList"] == true || e["ShoInList"] == "true")
-        .toList();
-    await _getCompanies();
-    EasyLoading.dismiss();
-    setState(() {});
-  }
+  // _getFormCompany() async {
+  //   EasyLoading.show();
+  //   filteredCompanies.clear();
+  //   companies.clear();
+  //   final result = await cdiRepository.fetchDataTable(COMPANY_ENTITY);
+  //   columnsData = result
+  //       .where((e) => e["ShowInList"] == true || e["ShoInList"] == "true")
+  //       .toList();
+  //   await _getCompanies();
+  //   EasyLoading.dismiss();
+  //   setState(() {});
+  // }
 
   @override
   void initState() {
     super.initState();
-    _getFormCompany();
+    // _getFormCompany();
   }
 
   @override
@@ -68,111 +71,22 @@ class _ListCompaniesState extends State<ListCompanies> {
     EasyLoading.dismiss();
   }
 
-  _handleManageCompany(int? companyId) async {
-    final needUpdateListCompanies = await Get.toNamed(
-        RouterPaths.MANAGE_COMPANY_PAGE,
-        arguments: {"companyId": companyId});
-    if (needUpdateListCompanies) {
-      _getFormCompany();
-    }
-  }
+  // _handleManageCompany(int? companyId) async {
+  //   final needUpdateListCompanies = await Get.toNamed(
+  //       RouterPaths.MANAGE_COMPANY_PAGE,
+  //       arguments: {"companyId": companyId});
+  //   if (needUpdateListCompanies) {
+  //     _getFormCompany();
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Layout(
-        sideBarList: [],
-        appBar: CustomAppBarSideBar(
-          title: "Empresas",
-          rightActions: [
-            IconButton(
-                icon: Icon(
-                  Icons.add_circle_sharp,
-                  color: AppColors.softMainColor,
-                  size: Dimensions.topIconSizeH,
-                ),
-                onPressed: () {
-                  _handleManageCompany(null);
-                })
-          ],
-        ),
-        child: Column(
-          children: [
-            FilterBox(
-              label: "Buscar Empresas",
-              hint: "Buscar",
-              elements: companies,
-              isLoading: false,
-              handleFilteredData: (List<dynamic> data) =>
-                  setState(() => filteredCompanies = data),
-            ),
-            if (columnsData.length > 0)
-              SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: CustomDataTable(
-                    columns: [...columnsData]
-                        .map((e) => e["HintText"].toString())
-                        .toList()
-                      ..add(""),
-                    elements: filteredCompanies
-                        .asMap()
-                        .map((index, element) => MapEntry(
-                            index,
-                            DataRow(
-                              cells: [...columnsData]
-                                  .map((e) => DataCell(
-                                      Text(element[e["bodyKey"]].toString())))
-                                  .toList()
-                                ..add(
-                                  DataCell(
-                                    Row(
-                                      children: [
-                                        IconButton(
-                                            onPressed: () =>
-                                                _handleManageCompany(
-                                                    element["id"]),
-                                            icon: Icon(Icons.edit_square)),
-                                        IconButton(
-                                            onPressed: () =>
-                                                _dialogDeleteCompany(
-                                                    context,
-                                                    element[columnsData[0]
-                                                            ["bodyKey"]]
-                                                        .toString(),
-                                                    element["id"]),
-                                            icon: Icon(Icons.delete))
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              color: index % 2 == 0
-                                  ? MaterialStateProperty.all<Color>(
-                                      AppColors.lightColor)
-                                  : MaterialStateProperty.all<Color>(
-                                      AppColors.lightSecondaryColor),
-                            )))
-                        .values
-                        .toList(),
-                  )),
-          ],
-        ));
-  }
-
-  _dialogDeleteCompany(BuildContext context, String name, int id) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return PopScope(
-          child: DeleteCompanyDialog(
-            companyName: name,
-            companyId: id,
-          ),
-        );
-      },
-    ).then((value) {
-      if (value == true) {
-        _getCompanies();
-        return;
-      }
-    });
+    return dynamicTableWidget(
+        route: RouterPaths.MANAGE_COMPANY_PAGE,
+        entity: COMPANY_ENTITY,
+        endpointRoute: COMPANY_ENDPOINT,
+        filterBoxLabel: "Empresas",
+        filterHintLabel: "Empresas");
   }
 }
--- lib/widgets/CDI/dynamic_table_widget.dart
+++ lib/widgets/CDI/dynamic_table_widget.dart
@@ -2,14 +2,17 @@ import 'package:developer_company/data/implementations/CDI/cdi_repository_impl.d
 import 'package:developer_company/data/providers/CDI/cdi_provider.dart';
 import 'package:developer_company/data/repositories/CDI/cdi_repository.dart';
 import 'package:developer_company/shared/resources/colors.dart';
+import 'package:developer_company/shared/resources/dimensions.dart';
+import 'package:developer_company/widgets/app_bar_sidebar.dart';
 import 'package:developer_company/widgets/data_table.dart';
 import 'package:developer_company/widgets/delete_company_dialog.dart';
 import 'package:developer_company/widgets/filter_box.dart';
+import 'package:developer_company/widgets/layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 
-class dynamicTable extends StatefulWidget {
+class dynamicTableWidget extends StatefulWidget {
   final String route;
   final String entity;
   final String endpointRoute;
@@ -17,7 +20,7 @@ class dynamicTable extends StatefulWidget {
   final String filterHintLabel;
 
 
-  const dynamicTable(
+  const dynamicTableWidget(
       {Key? key,
       required this.route,
       required this.entity,
@@ -28,16 +31,16 @@ class dynamicTable extends StatefulWidget {
       : super(key: key);
 
   @override
-  _dynamicTableState createState() => _dynamicTableState();
+  _dynamicTableWidgetState createState() => _dynamicTableWidgetState();
 }
 
-class _dynamicTableState extends State<dynamicTable> {
+class _dynamicTableWidgetState extends State<dynamicTableWidget> {
   CDIRepository cdiProvider = CDIRepositoryImpl(CDIProvider());
   List<dynamic> data = [];
   List<dynamic> filteredData = [];
 
   getData() async {
-    final tempData = await cdiProvider.fetchDataList(widget.entity);
+    final tempData = await cdiProvider.fetchDataList(widget.endpointRoute);
     data = tempData;
     filteredData = tempData;
   }
@@ -71,7 +74,7 @@ class _dynamicTableState extends State<dynamicTable> {
     EasyLoading.dismiss();
   }
 
-  _handleManageData(int? id) async {
+  handleManageData(int? id) async {
     final needUpdateListData =
         await Get.toNamed(widget.route, arguments: {"dataId": id});
     if (needUpdateListData) {
@@ -81,64 +84,81 @@ class _dynamicTableState extends State<dynamicTable> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        FilterBox(
-          label: widget.filterBoxLabel,
-          hint: widget.filterHintLabel,
-          elements: data,
-          isLoading: false,
-          handleFilteredData: (List<dynamic> data) =>
-              setState(() => filteredData = data),
+    return Layout(
+        sideBarList: [],
+        appBar: CustomAppBarSideBar(
+          title: "Empresas",
+          rightActions: [
+            IconButton(
+                icon: Icon(
+                  Icons.add_circle_sharp,
+                  color: AppColors.softMainColor,
+                  size: Dimensions.topIconSizeH,
+                ),
+                onPressed: () =>
+                  handleManageData(null)
+                )
+          ],
         ),
-        if (columnsData.length > 0)
-          SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: CustomDataTable(
-                columns: [...columnsData]
-                    .map((e) => e["HintText"].toString())
-                    .toList()
-                  ..add(""),
-                elements: filteredData
-                    .asMap()
-                    .map((index, element) => MapEntry(
-                        index,
-                        DataRow(
-                          cells: [...columnsData]
-                              .map((e) => DataCell(
-                                  Text(element[e["bodyKey"]].toString())))
-                              .toList()
-                            ..add(
-                              DataCell(
-                                Row(
-                                  children: [
-                                    IconButton(
-                                        onPressed: () => _handleManageData(
-                                            element["id"]),
-                                        icon: Icon(Icons.edit_square)),
-                                    IconButton(
-                                        onPressed: () =>
-                                            _dialogDeleteDataById(
-                                                context,
-                                                element[columnsData[0]
-                                                        ["HintText"]!]
-                                                    .toString(),
-                                                element["id"]),
-                                        icon: Icon(Icons.delete))
-                                  ],
+      child: Column(
+        children: [
+          FilterBox(
+            label: widget.filterBoxLabel,
+            hint: widget.filterHintLabel,
+            elements: data,
+            isLoading: false,
+            handleFilteredData: (List<dynamic> data) =>
+                setState(() => filteredData = data),
+          ),
+          if (columnsData.length > 0)
+            SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: CustomDataTable(
+                  columns: [...columnsData]
+                      .map((e) => e["HintText"].toString())
+                      .toList()
+                    ..add(""),
+                  elements: filteredData
+                      .asMap()
+                      .map((index, element) => MapEntry(
+                          index,
+                          DataRow(
+                            cells: [...columnsData]
+                                .map((e) => DataCell(
+                                    Text(element[e["bodyKey"]].toString())))
+                                .toList()
+                              ..add(
+                                DataCell(
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                          onPressed: () => handleManageData(
+                                              element["id"]),
+                                          icon: Icon(Icons.edit_square)),
+                                      IconButton(
+                                          onPressed: () =>
+                                              _dialogDeleteDataById(
+                                                  context,
+                                                  element[columnsData[0]
+                                                          ["HintText"]!]
+                                                      .toString(),
+                                                  element["id"]),
+                                          icon: Icon(Icons.delete))
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          color: index % 2 == 0
-                              ? MaterialStateProperty.all<Color>(
-                                  AppColors.lightColor)
-                              : MaterialStateProperty.all<Color>(
-                                  AppColors.lightSecondaryColor),
-                        )))
-                    .values
-                    .toList(),
-              )),
-      ],
+                            color: index % 2 == 0
+                                ? MaterialStateProperty.all<Color>(
+                                    AppColors.lightColor)
+                                : MaterialStateProperty.all<Color>(
+                                    AppColors.lightSecondaryColor),
+                          )))
+                      .values
+                      .toList(),
+                )),
+        ],
+      ),
     );
   }
 
